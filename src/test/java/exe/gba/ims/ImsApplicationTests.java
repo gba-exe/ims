caf79f70a09b33325b47e6cf8c2eb7e29be5b951
gba-exe <gabriel.baraujo@sptech.school>
2024-04-08 17:15:44 -0300

    Initial Commit

---
+++ src/test/java/exe/gba/ims/ImsApplicationTests.java
@@ -0,0 +1,13 @@
+package exe.gba.ims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
